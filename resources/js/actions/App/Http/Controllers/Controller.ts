698abac105e0df1d73c652fe05716aca06b8cd4a
mfauzanhanif <fauzanhanif2112@gmail.com>
2026-02-26 07:34:03 +0700

    want to merge

--- resources/js/actions/App/Http/Controllers/Controller.ts
+++ resources/js/actions/App/Http/Controllers/Controller.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Controller::beranda
  * @see app/Http/Controllers/Controller.php:9
--- resources/js/routes/PPDB/index.ts
+++ resources/js/routes/PPDB/index.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
 /**
 * @see \App\Http\Controllers\PPDBController::index
  * @see app/Http/Controllers/PPDBController.php:15
