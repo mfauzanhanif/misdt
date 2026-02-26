c8886d772434baf6c6463a1e9f8aa73070d4d226
mfauzanhanif <fauzanhanif2112@gmail.com>
2026-02-26 07:39:39 +0700

    redeploy

--- resources/js/actions/App/Http/Controllers/Controller.ts
+++ resources/js/actions/App/Http/Controllers/Controller.ts
@@ -1,5 +1,4 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../wayfinder'
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Controller::beranda
  * @see app/Http/Controllers/Controller.php:9
--- resources/js/routes/PPDB/index.ts
+++ resources/js/routes/PPDB/index.ts
@@ -1,5 +1,4 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
 /**
 * @see \App\Http\Controllers\PPDBController::index
  * @see app/Http/Controllers/PPDBController.php:13
